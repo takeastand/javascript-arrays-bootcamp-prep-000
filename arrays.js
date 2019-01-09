d802a2073ef5bf1546a3c30ef270599f121ee098
christopher.tero Tero <christopher.tero@gmail.com>
2019-01-09 02:41:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,4 +28,6 @@ function removeElementFromBeginningOfArray(array) {
   
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array)
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
